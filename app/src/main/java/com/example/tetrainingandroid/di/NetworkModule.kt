8feb51a0d93c92cba07413abd2ad47e32d7ffedc
nhan.duong <nhan.duong@executionlab.asia>
2021-09-14 13:53:21 +0700

    add TMDB base url & authentication interceptor

--- app/src/main/java/com/example/tetrainingandroid/di/NetworkModule.kt
+++ app/src/main/java/com/example/tetrainingandroid/di/NetworkModule.kt
@@ -1,12 +1,14 @@
 package com.example.tetrainingandroid.di
 
 import com.example.tetrainingandroid.config.Config
+import com.example.tetrainingandroid.data.interceptor.AuthenticationInterceptor
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -26,8 +28,9 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideClient(): OkHttpClient {
+    fun provideClient(interceptor: AuthenticationInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
             .readTimeout(Config.READ_TIMEOUT, TimeUnit.MILLISECONDS)
             .connectTimeout(Config.CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
             .build()
