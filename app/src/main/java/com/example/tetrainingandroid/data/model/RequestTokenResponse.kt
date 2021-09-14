97463447fc9a51e46bad8ac852cba4b52760eb7d
nhan.duong <nhan.duong@executionlab.asia>
2021-09-14 17:04:36 +0700

    Testing working

--- app/src/main/java/com/example/tetrainingandroid/data/model/RequestTokenResponse.kt
+++ app/src/main/java/com/example/tetrainingandroid/data/model/RequestTokenResponse.kt
@@ -1,4 +1,10 @@
 package com.example.tetrainingandroid.data.model
 
-class RequestTokenResponse {
-}
+import com.squareup.moshi.Json
+import java.util.*
+
+data class RequestTokenResponse(
+    @Json(name = "success") val success: Boolean?,
+    @Json(name = "expires_at") val expiresAt: Date?,
+    @Json(name = "request_token") val requestToken: String?,
+)
--- app/src/main/java/com/example/tetrainingandroid/data/service/TestService.kt
+++ app/src/main/java/com/example/tetrainingandroid/data/service/TestService.kt
@@ -1,4 +1,11 @@
 package com.example.tetrainingandroid.data.service
 
+import com.example.tetrainingandroid.data.model.RequestTokenResponse
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface TestService {
+    @GET("/3/authentication/token/new")
+    suspend fun getMovies(): RequestTokenResponse
 }
