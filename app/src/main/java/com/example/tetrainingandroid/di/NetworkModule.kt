23ebdd5856dcea994462de3c5036573d73745620
nhan.duong <nhan.duong@executionlab.asia>
2021-09-14 10:35:06 +0700

    add network module

---
+++ app/src/main/java/com/example/tetrainingandroid/di/NetworkModule.kt
@@ -0,0 +1,45 @@
+package com.example.tetrainingandroid.di
+
+import com.example.tetrainingandroid.config.Config
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory::class)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(Config.READ_TIMEOUT, TimeUnit.MILLISECONDS)
+            .connectTimeout(Config.CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(Config.BASE_URL)
+            .build()
+    }
+}
