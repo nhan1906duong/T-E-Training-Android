b6aacdd273ed058fa097c3293931ece34b0d695c
nhan.duong <nhan.duong@executionlab.asia>
2021-09-18 21:41:08 +0700

    remove unused extensions

--- app/src/main/java/com/example/tetrainingandroid/architecture/CacheViewFragment.kt
+++ app/src/main/java/com/example/tetrainingandroid/architecture/CacheViewFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import com.example.tetrainingandroid.extensions.removeFromParent
 
 open class CacheViewFragment(@LayoutRes contentLayoutId: Int): BaseFragment(contentLayoutId)  {
     private var rootView: View? = null
@@ -18,7 +17,7 @@ open class CacheViewFragment(@LayoutRes contentLayoutId: Int): BaseFragment(cont
         if(rootView == null) {
             rootView = super.onCreateView(inflater, container, savedInstanceState)
         } else {
-            rootView?.removeFromParent()
+            (rootView?.parent as? ViewGroup)?.removeView(rootView)
         }
         return rootView
     }
