5c6bb077fdd3981e62a9c3095a908162378f283c
nhan.duong <nhan.duong@executionlab.asia>
2021-09-25 10:48:05 +0700

    Set current item when backstack Main ViewPager

--- app/src/main/java/com/example/tetrainingandroid/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/tetrainingandroid/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.tetrainingandroid.ui.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.doOnPreDraw
 import com.example.tetrainingandroid.R
 import com.example.tetrainingandroid.architecture.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,27 +13,34 @@ import javax.inject.Inject
 class MainFragment: BaseFragment(R.layout.main_fragment) {
     @Inject lateinit var viewPagerAdapter: MainFragmentStateAdapter
 
+    private var currentIndex = 0
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpViewPager()
         setupBottomNavigationBar()
     }
 
-    override fun onStart() {
-        super.onStart()
-        pager?.adapter = viewPagerAdapter
-    }
-
     override fun onStop() {
         super.onStop()
+        currentIndex = pager?.currentItem ?: 0
         removeViewPagerAdapter() // before Fragment save view instance state
     }
 
     private fun setUpViewPager() {
         pager?.offscreenPageLimit = 4
-        pager?.adapter = viewPagerAdapter
-        pager?.isUserInputEnabled = false
+        if (pager?.adapter == null) {
+            pager?.adapter = viewPagerAdapter
+            if (currentIndex != 0) {
+                pager?.doOnPreDraw {
+                    pager?.currentItem = currentIndex
+                } // or use post (ViewTreeObserver)
+            }
+        }
+        disableViewPagerSwipe()
     }
+
+    private fun disableViewPagerSwipe() = false.also { pager?.isUserInputEnabled = it }
 
     private fun setupBottomNavigationBar() {
         bottomNavigationBar?.setOnItemSelectedListener { item ->
